2e02dbd2ff844f5cc3166109987d60c59630646f
James Anhalt III <jeaiii@anhalt.org>
2017-11-11 11:04:06 -0800

    marco and template versions split files

---
+++ itoa/itoa_jeaiii.cpp
@@ -0,0 +1,98 @@
+#include <stdint.h>
+
+template <int N> __forceinline void digits(char *b, uint64_t t)
+{
+    b[0] = char(t >> 32) + '0';
+    digits<N - 1>(++b, 10ULL * uint32_t(t));
+}
+
+#define Z(d) (void)(*(uint16_t*)b = uint16_t(d) + '0')
+
+template <> __forceinline void digits<0>(char *b, uint64_t t)
+{
+    Z(t >> 32);
+}
+
+#define A(N) digits<N>(b, ((1ULL << 32) / uint32_t(1e##N) + 1) * i)
+#define B(N) digits<N>(b, (((1ULL << (32+N*3)) / uint32_t(1e##N) + 1) * i + (1ULL << (N*3))) >> (N*3))
+
+void xu32toa_jeaiii(uint32_t i, char *b)
+{
+    i < 100 ?
+        i < 10 ? Z(i) : A(1) :
+        i < 1000000 ?
+        i < 10000 ?
+        i < 1000 ? A(2) : A(3) :
+        i < 100000 ? A(4) : B(5) :
+        i < 100000000 ?
+        i < 10000000 ? B(6) : B(7) :
+        i < 1000000000 ? B(8) : digits<9>(b, ((1ULL << (32 + 29)) / uint32_t(1e9) * i + (1ULL << 31)) >> 29);
+}
+
+#define TX *b++ = char(t >> 32) + '0', t = 10ULL * uint32_t(t)
+#define T0(I) *(uint16_t*)b++ = uint16_t(I) + '0', (void)0
+#define T1 TX, T0(t >> 32)
+#define T2 TX, T1
+#define T3 TX, T2
+#define T4 TX, T3
+#define T5 TX, T4
+#define T6 TX, T5
+#define T7 TX, T6
+#define T8 TX, T7
+#define T9 TX, T8
+#define T(N,B) (t = (1ULL << (32 + B)) / uint32_t(1e##N) + 1, t *= i, t += !!B << (B), t >>= B, T##N)
+
+void u32toa_jeaiii(uint32_t i, char *b)
+{
+    uint64_t t;
+
+    i < 100 ?
+        i < 10 ? T0(i) : T(1, 0) :
+        i < 1000000 ?
+        i < 10000 ?
+        i < 1000 ? T(2, 0) : T(3, 0) :
+        i < 100000 ? T(4, 0) : T(5, 15) :
+        i < 100000000 ?
+        i < 10000000 ? T(6, 18) : T(7, 21) :
+        i < 1000000000 ? T(8, 24) : (t = (1LL << (32 + 29)) / uint32_t(1e9) + 0, t *= i, t += 1ULL << 31, t >>= 29, T9);
+}
+
+void i32toa_jeaiii(int32_t i, char* b)
+{
+    if (i < 0)
+        *b++ = '-', i = -i;
+    u32toa_jeaiii(uint32_t(i), b);
+}
+
+
+void u64toa_jeaiii(uint64_t n, char* b)
+{
+    if (uint32_t(n >> 32) == 0)
+        return u32toa_jeaiii(uint32_t(n), b);
+
+    uint64_t t = n / 100000000;
+
+    if (uint32_t(t >> 32) == 0)
+    {
+        u32toa_jeaiii(uint32_t(t), b);
+        while (*++b);
+    }
+    else
+    {
+        u32toa_jeaiii(uint32_t(t / 100000000), b);
+        while (*++b);
+        uint32_t i = t % 100000000;
+        T(7, 21);
+    }
+
+    uint32_t i = n % 100000000;
+    T(7, 21);
+}
+
+void i64toa_jeaiii(int64_t i, char* b)
+{
+    if (i < 0)
+        *b++ = '-', i = -i;
+    u64toa_jeaiii(uint64_t(i), b);
+}
+
--- itoa/main.cpp
+++ itoa/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 
+#if 0
 #define D(N,B) { b += N; t = (1LL << (32 + B)) / uint32_t(1e##N) + 1; t *= i; (B) && (t += 1LL << (B)); i = uint32_t(t >> (32 + B)); n = uint32_t(t >> (B)); goto i##N; }
 
 char* itoa(char *b, uint32_t i)
@@ -21,54 +22,77 @@ char* itoa(char *b, uint32_t i)
         {
             if (i < 1000)
                 D(2, 0)
-            D(3, 0)
+                D(3, 0)
         }
         if (i < 100000)
-            D(4,0)
-        D(5, 15)
+            D(4, 0)
+            D(5, 15)
     }
 
     if (i < 100000000)
     {
         if (i < 10000000)
             D(6, 18)
-        D(7, 21)
+            D(7, 21)
     }
 
     if (i < 1000000000)
         D(8, 24)
 
-    { b += 9; t = (1LL << (32 + 29)) / uint32_t(1e9) + 0; t *= i; t += 1LL << (31); i = uint32_t(t >> (32 + 29)); n = uint32_t(t >> (29)); }
+    {
+        b += 9; t = (1LL << (32 + 29)) / uint32_t(1e9) + 0; t *= i; t += 1LL << (31); i = uint32_t(t >> (32 + 29)); n = uint32_t(t >> (29));
+    }
 
 #define T { t = 10; t *= n; i = uint32_t(t >> 32); n = uint32_t(t); }
 
     b[-9] = i + '0'; T
-i8: b[-8] = i + '0'; T
-i7: b[-7] = i + '0'; T
-i6: b[-6] = i + '0'; T
-i5: b[-5] = i + '0'; T
-i4: b[-4] = i + '0'; T
-i3: b[-3] = i + '0'; T
-i2: b[-2] = i + '0'; T
-i1: b[-1] = i + '0'; T
-i0: b[ 0] = i + '0';
-    b[ 1] = 0;
+        i8 : b[-8] = i + '0'; T
+        i7 : b[-7] = i + '0'; T
+        i6 : b[-6] = i + '0'; T
+        i5 : b[-5] = i + '0'; T
+        i4 : b[-4] = i + '0'; T
+        i3 : b[-3] = i + '0'; T
+        i2 : b[-2] = i + '0'; T
+        i1 : b[-1] = i + '0'; T
+        i0 : b[0] = i + '0';
+    b[1] = 0;
 
-    return b+1;
+    return b + 1;
 }
 
 char* itoa(char* b, int32_t i)
 {
-    if (i < 0) 
-        *b++ = '-', i=-i;
+    if (i < 0)
+        *b++ = '-', i = -i;
     return itoa(b, uint32_t(i));
 }
+#endif
+
+extern void i32toa_jeaiii(int32_t i, char* b);
+extern void u32toa_jeaiii(uint32_t i, char* b);
+
+void itoa(char* b, int32_t i) { return i32toa_jeaiii(i, b); }
+void itoa(char* b, uint32_t i) { return u32toa_jeaiii(i, b); }
 
 bool same(uint32_t i)
 {
-    char text[32];
-    itoa(text, i);
-    return std::to_string(i) == text;
+    char a[32];
+    itoa(a, i);
+
+    char b[32], *c=b+32;
+
+    *--c = 0;
+    do {
+        *--c = i % 10 + '0';
+    } while (i /= 10);
+
+    if (strcmp(a, c)) {
+        std::cout << a << "!=" << c << '\n';
+        return false;
+    }
+    return true;
+
+    //return std::to_string(i) == text;
 }
 
 void main()
@@ -81,7 +105,7 @@ void main()
         if (!same(n))
             itoa(text, n), std::cout << i << "!=" << text << "\n", __debugbreak();
         i += 1;
-    } while (i < 1LL<<32);
+    } while (i < 1LL << 32);
 
     itoa(text, 0); std::cout << text << "\n";
     itoa(text, 1); std::cout << text << "\n";
@@ -106,6 +130,6 @@ void main()
     itoa(text, 1000000000); std::cout << text << "\n";
     itoa(text, 4294967295U); std::cout << text << "\n";
     itoa(text, -1); std::cout << text << "\n";
-    itoa(text, 1<<31); std::cout << text << "\n";
+    itoa(text, 1 << 31); std::cout << text << "\n";
     itoa(text, 0x7fffffff); std::cout << text << "\n";
 }
