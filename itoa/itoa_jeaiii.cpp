09e91149b3dd354aa7cfecefed33017671e8332e
jeaiii <jeaiii@anhalt.org>
2017-11-11 16:36:08 -0800

    Update itoa_jeaiii.cpp

--- itoa/itoa_jeaiii.cpp
+++ itoa/itoa_jeaiii.cpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2017 jeaiii
+Copyright (c) 2017 James Edward Anhalt III (jeaiii)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
