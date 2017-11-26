c4c1a3d7baa4381b2695f4aaf57e1641ad94916d
James Anhalt III <jeaiii@anhalt.org>
2017-11-25 23:10:06 -0800

    faster check, cleanup

--- itoa/itoa_jeaiii.cpp
+++ itoa/itoa_jeaiii.cpp
@@ -1,8 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2017 James Edward Anhalt III (jeaiii)
-https://github.com/jeaiii/itoa
+Copyright (c) 2017 James Edward Anhalt III - https://github.com/jeaiii/itoa
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- itoa/main.cpp
+++ itoa/main.cpp
@@ -1,8 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2017 James Edward Anhalt III (jeaiii)
-https://github.com/jeaiii/itoa
+Copyright (c) 2017 James Edward Anhalt III - https://github.com/jeaiii/itoa
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -34,36 +33,30 @@ void u32toa_jeaiii(uint32_t i, char* b);
 void itoa(char* b, int32_t i) { i32toa_jeaiii(i, b); }
 void itoa(char* b, uint32_t i) { u32toa_jeaiii(i, b); }
 
-bool same(uint32_t i)
-{
-    char a[32];
-    itoa(a, i);
-#if 1
-    char b[32], *c=b+32;
-
-    *--c = 0;
-    uint32_t n = i;
-    do {
-        *--c = i % 10 + '0';
-    } while (i /= 10);
-    i = n;
-
-    if (strcmp(a, c)) {
-#else
-    std::string c = std::to_string(i);
-    if (a != c) {
-#endif
-        std::cout << "FAILURE: " << a << " != " << i << " : " << c << "\n";
-        __debugbreak();
-        return false;
-    }
-    return true;
+bool check(const char* b, uint32_t n) {
+    uint32_t u = 0;
+    for (; *b; ++b) u = u * 10 + (*b - '0');
+    return u == n;
 }
 
-void main()
+void same(uint32_t n)
 {
     char text[32];
+    itoa(text, n);
+    if (!check(text, n)) {
+        std::cout << "FAILURE: " << text << " != " << n << "\n";
+    }
+}
 
+template <typename T>
+void show(T n) {
+    char text[32];
+    itoa(text, n);
+    std::cout << text << "\n";
+}
+
+int main()
+{
     uint64_t i = 0;
     do {
         uint32_t n = uint32_t(i);
@@ -71,29 +64,32 @@ void main()
         i += 1;
     } while (i < 1ULL << 32);
 
-    itoa(text, 0); std::cout << text << "\n";
-    itoa(text, 1); std::cout << text << "\n";
-    itoa(text, 9); std::cout << text << "\n";
-    itoa(text, 10); std::cout << text << "\n";
-    itoa(text, 99); std::cout << text << "\n";
-    itoa(text, 100); std::cout << text << "\n";
-    itoa(text, 999); std::cout << text << "\n";
-    itoa(text, 1000); std::cout << text << "\n";
-    itoa(text, 9999); std::cout << text << "\n";
-    itoa(text, 10000); std::cout << text << "\n";
-    itoa(text, 99999); std::cout << text << "\n";
-    itoa(text, 100000); std::cout << text << "\n";
-    itoa(text, 999999); std::cout << text << "\n";
-    itoa(text, 1000000); std::cout << text << "\n";
-    itoa(text, 9999999); std::cout << text << "\n";
-    itoa(text, 10000000); std::cout << text << "\n";
-    itoa(text, 99999999); std::cout << text << "\n";
-    itoa(text, 100000000); std::cout << text << "\n";
-    itoa(text, 123456789); std::cout << text << "\n";
-    itoa(text, 999999999); std::cout << text << "\n";
-    itoa(text, 1000000000); std::cout << text << "\n";
-    itoa(text, 4294967295U); std::cout << text << "\n";
-    itoa(text, -1); std::cout << text << "\n";
-    itoa(text, 1 << 31); std::cout << text << "\n";
-    itoa(text, 0x7fffffff); std::cout << text << "\n";
+    show(0);
+    show(1);
+    show(9);
+    show(10);
+    show(99);
+    show(100);
+    show(999);
+    show(1000);
+    show(9999);
+    show(10000);
+    show(99999);
+    show(100000);
+    show(999999);
+    show(1000000);
+    show(9999999);
+    show(10000000);
+    show(99999999);
+    show(100000000);
+    show(123456789);
+    show(999999999);
+    show(1000000000);
+    show(4294967295U);
+    show(-1);
+    show(1 << 31);
+    show(0x7fffffff);
+    show(-0x7fffffff - 1);
+
+    return 0;
 }
